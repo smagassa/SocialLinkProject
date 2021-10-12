d14a7f712c39534d0f94089897fa54f4cd4c83db
smagassa <92060081+smagassa@users.noreply.github.com>
2021-10-11 21:46:58 -0400

    Add files via upload

---
+++ login.py
@@ -0,0 +1,17 @@
+print ("Hello world222")
+
+import django
+print(django.get_version())
+print(django._author_)
+
+
+ def logout_request(request):
+      logout(request)
+      return redirect("")
+
+py manage.py  createsuperuser 
+1=enter your userName
+2=enter your email
+3=enter your password
+4=enter your password again 
+5=for create user enter y (yes)
